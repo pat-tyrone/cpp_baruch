05084329c35eccaa43c249109e397c83a7665fd5
Patrick Cannon <patrickcannon805@gmail.com>
2025-11-20 22:06:43 -0500

    new project for level 1.3

---
+++ cpp_baruch/sandbox/hello_world.cpp
@@ -0,0 +1,26 @@
+// Hello.cpp
+//
+// Simple example of function bodies.
+//
+
+
+// Preprocessor for include files
+#include <stdio.h>			// C style I/O
+#include <iostream>			// C++ style I/O using operator overloading
+using namespace std;		// The C++ logical collection of functions
+
+void hello_C()
+{
+	printf("Hello Quantnet, C style\n");
+}
+
+void hello_CPP()
+{
+	cout << "Hello Quantnet, C++ style \n";
+}
+
+void main()
+{
+	hello_C();
+	hello_CPP();
+}
